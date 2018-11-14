38a8070dec29bc4def60daa0d82a04fc79f159bc
proelbtn <proelbtn@users.noreply.github.com>
2018-11-14 15:48:19 +0900

    implement other function, val2ang, ang2val

--- no6/utils.ino
+++ no6/utils.ino
@@ -13,3 +13,11 @@ double ang2rad(double ang) {
 int rad2val(double rad) {
   return (int)(rad / (PI / 180.) / 150. * 512.) + 512;
 }
+
+double val2ang(int val) {
+  return rad2ang(val2rad(val));
+}
+
+int ang2val(double ang) {
+  return rad2val(ang2rad(ang));
+}
